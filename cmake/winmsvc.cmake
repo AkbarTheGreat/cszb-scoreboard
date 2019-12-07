ef73a3a862268d944f1963c874bf21930332ddf0
Tracy Beck <tgbeck@acm.org>
2019-12-07 10:16:11 -0500

    Adding protobuf compilation and a trivial proto file which will probably change before actually being used.

--- cmake/winmsvc.cmake
+++ cmake/winmsvc.cmake
@@ -47,27 +47,76 @@ else()
     add_definitions(-D_UNICODE -DUNICODE -DWXUSINGDLL -DwxUSE_GUI=1 -D__WXMSW__)
 endif()
 
+set(_VCPKG_PROTOBUF_DIR ${_VCPKG_INSTALLED_DIR}/../packages/protobuf_x64-windows)
+
+include_directories(${_VCPKG_PROTOBUF_DIR}/include)
 if("${CMAKE_BUILD_TYPE}" MATCHES "Debug")
 	add_definitions(-D_DEBUG)
 	include_directories(${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/mswud ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
-	set(wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib)
+	set(wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug)
+	set(protobuf_LIB_DIR ${_VCPKG_PROTOBUF_DIR}/debug)
+	if(LINKING_TYPE MATCHES "static")
+		set(wxWidgets_LIBRARIES 
+			${wxWidgets_LIB_DIR}/lib/wxbase31ud_net.lib
+			${wxWidgets_LIB_DIR}/lib/wxmsw31ud_core.lib
+			${wxWidgets_LIB_DIR}/lib/wxbase31ud.lib
+			${wxWidgets_LIB_DIR}/lib/jpegd.lib
+			${wxWidgets_LIB_DIR}/lib/libpng16d.lib
+			${wxWidgets_LIB_DIR}/lib/lzmad.lib
+			${wxWidgets_LIB_DIR}/lib/tiffd.lib
+			${wxWidgets_LIB_DIR}/lib/zlibd.lib
+		)
+		set(protobuf_LIBRARIES
+			${protobuf_LIB_DIR}/lib/libprotobufd.lib
+		)
+	else()
+		set(wxWidgets_LIBRARIES
+			${wxWidgets_LIB_DIR}/bin/wxbase313ud_net_vc_x64_custom.dll
+			${wxWidgets_LIB_DIR}/bin/wxmsw313ud_core_vc_x64_custom.dll
+			${wxWidgets_LIB_DIR}/bin/wxbase313ud_vc_x64_custom.dll
+			${wxWidgets_LIB_DIR}/bin/libpng16d.dll
+			${wxWidgets_LIB_DIR}/bin/zlibd1.dll
+		)
+		set(protobuf_LIBRARIES
+			${protobuf_LIB_DIR}/bin/libprotobufd.dll
+		)
+	endif()
 	set(wxWidgets_LIBRARIES
-		${wxWidgets_LIB_DIR}/wxbase31ud_net.lib
-		${wxWidgets_LIB_DIR}/wxmsw31ud_core.lib
-		${wxWidgets_LIB_DIR}/wxbase31ud.lib
-        ${wxWidgets_LIB_DIR}/libpng16d.lib
-        ${wxWidgets_LIB_DIR}/zlibd.lib
+  		${wxWidgets_LIBRARIES}
         comctl32 Rpcrt4
 		)
 elseif("${CMAKE_BUILD_TYPE}" MATCHES "Release")
 	include_directories(${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/mswu ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
-	set(wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
+	set(wxWidgets_LIB_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET})
+	set(protobuf_LIB_DIR ${_VCPKG_PROTOBUF_DIR})
+	if(LINKING_TYPE MATCHES "static")
+		set(wxWidgets_LIBRARIES 
+			${wxWidgets_LIB_DIR}/lib/wxbase31u_net.lib
+			${wxWidgets_LIB_DIR}/lib/wxmsw31u_core.lib
+			${wxWidgets_LIB_DIR}/lib/wxbase31u.lib
+			${wxWidgets_LIB_DIR}/lib/jpeg.lib
+			${wxWidgets_LIB_DIR}/lib/libpng16.lib
+			${wxWidgets_LIB_DIR}/lib/lzma.lib
+			${wxWidgets_LIB_DIR}/lib/tiff.lib
+			${wxWidgets_LIB_DIR}/lib/zlib.lib
+		)
+		set(protobuf_LIBRARIES
+			${protobuf_LIB_DIR}/lib/libprotobuf.lib
+		)
+	else()
+		set(wxWidgets_LIBRARIES
+			${wxWidgets_LIB_DIR}/bin/wxbase313u_net_vc_x64_custom.dll
+			${wxWidgets_LIB_DIR}/bin/wxmsw313u_core_vc_x64_custom.dll
+			${wxWidgets_LIB_DIR}/bin/wxbase313u_vc_x64_custom.dll
+			${wxWidgets_LIB_DIR}/bin/libpng16.dll
+			${wxWidgets_LIB_DIR}/bin/zlib1.dll
+		)
+		set(protobuf_LIBRARIES
+			${protobuf_LIB_DIR}/bin/libprotobuf.dll
+		)
+	endif()
 	set(wxWidgets_LIBRARIES
-		${wxWidgets_LIB_DIR}/wxbase31u_net.lib
-		${wxWidgets_LIB_DIR}/wxmsw31u_core.lib
-		${wxWidgets_LIB_DIR}/wxbase31u.lib
-        ${wxWidgets_LIB_DIR}/libpng16.lib
-        ${wxWidgets_LIB_DIR}/zlib.lib
+  		${wxWidgets_LIBRARIES}
         comctl32 Rpcrt4
 		)
 endif()
