f77e5840b243f83ee57cb0ca3319204b11340dd7
Tracy Beck <tgbeck@acm.org>
2020-02-28 22:59:05 -0500

    Version auto-commit via release script

--- include/ScoreboardCommon.h
+++ include/ScoreboardCommon.h
@@ -24,7 +24,7 @@ namespace cszb_scoreboard {
 
 const int DEFAULT_BORDER_SIZE = 5;
 
-#define SCOREBOARD_VERSION "0.0.3"
+#define SCOREBOARD_VERSION "0.5.0"
 
 // Evaluates to public: in a test environment, private: in non-test code, to
 // hide dependencies as tightly as possible
