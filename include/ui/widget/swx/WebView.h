25af5c31cd4d4bd545c9c2028d656f0cfb296208
Tracy Beck <tgbeck@acm.org>
2022-10-09 10:29:04 -0400

    Add a default implementation of a web browser for platforms that aren't win32.  This may not actually do much, but is more correct than insisting on Edge regardless of platform.

--- include/ui/widget/swx/WebView.h
+++ include/ui/widget/swx/WebView.h
@@ -40,13 +40,14 @@ class WebView {
                    const wxString &backend = wxWebViewBackendDefault,
                    int64_t style = 0, const wxString &name = wxWebViewNameStr) {
 
-    _wx = wxWebView::New(parent, id, url, pos, size, wxWebViewBackendEdge, style, name);
-
-    // Log backend information
 #ifdef _WIN32
+    _wx = wxWebView::New(parent, id, url, pos, size, wxWebViewBackendEdge, style, name);
+    // Log backend information
     LogDebug("Backend: %s Version: %s", _wx->GetClassInfo()->GetClassName(),
              wxWebView::GetBackendVersionInfo().ToString());
     LogDebug("User Agent: %s", _wx->GetUserAgent());
+#else // #ifdef _WIN32
+    _wx = wxWebView::New(parent, id, url, pos, size, wxWebViewBackendDefault, style, name);
 #endif // #ifdef _WIN32
   }
 
