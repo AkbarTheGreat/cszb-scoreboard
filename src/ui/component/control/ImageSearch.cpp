662cf033ec2e5171b4f1b8a4afe790ee0cb8a5f1
Akbar <tgbeck@acm.org>
2022-10-11 18:14:37 -0400

    Fixing coverage builds.

--- src/ui/component/control/ImageSearch.cpp
+++ src/ui/component/control/ImageSearch.cpp
@@ -111,7 +111,7 @@ void ImageSearch::bindEvents() {
 void ImageSearch::onURLDrop(const std::string &url) {
   HttpReader reader;
   std::vector<char> image_data;
-  LogDebug("Url dropped: %s", url);
+  LogDebug("Url dropped: %s", url.c_str());
   reader.readBinary(url.c_str(), &image_data);
   Image urlImage = Image::FromData(image_data);
   if (screen_selection->allSelected()) {
