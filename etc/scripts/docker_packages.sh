4ae6f27391413bda6dd1a6036e08f1b6d0146491
Tracy Beck <tgbeck@acm.org>
2023-02-02 17:04:05 -0500

    Add a script which installs apt/perl packages centrally.  This is currently a mirror of what is done in the two dockerfiles, but additional tweaks will follow.

---
+++ etc/scripts/docker_packages.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+ export DEBIAN_FRONTEND=noninteractive
+
+/usr/bin/apt update
+/usr/bin/apt -y upgrade
+
+/usr/bin/apt -y install   \
+	clang \
+	clang-tidy \
+	cmake \
+	cmake-curses-gui \
+	g++ \
+	git \
+	gcovr \
+	libcairo2-dev \
+	libexpat1-dev \
+	libgtk-3-dev \
+	libjpeg-dev \
+	libjsoncpp-dev \
+	libmspack-dev \
+	libnotify-dev \
+	libpng-dev \
+	libprotobuf-dev \
+	libsdl1.2-dev \
+	libsdl2-dev \
+	libsecret-1-dev \
+	libsoup2.4-dev \
+	libssl-dev \
+	libtiff-dev \
+	libwebkit2gtk-4.0-dev \
+	libx11-dev \
+	llvm-dev \
+	ninja-build \
+	openjdk-17-jre \
+	protobuf-compiler \
+	tightvncserver \
+	valgrind
+
+if [[ "$1" == "osxcross" ]]; then
+	/usr/bin/apt -y install \
+		curl
+	echo "yes" | /usr/bin/cpan install \
+		File::Copy::Recursive \
+		File::Which \
+		List::AllUtils
+#else
+# TODO -- break out non-osx dependencies
+fi
