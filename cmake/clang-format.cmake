402be8b7f6ffc6840fa55115fbbf101171652da2
Tracy Beck <tgbeck@acm.org>
2023-02-26 08:40:08 -0500

    Fix the clangformat target we use, primarily around protobufs.  The new version no longer formats the generated .pb.h and .pb.cpp files in the out/ directory but it also finally actually formats the .proto files themselves, which is a thing that was always intended to be the case.

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,8 +1,12 @@
-# Taken from https://arcanis.me/en/2015/10/17/cppcheck-and-clang-format
+# Originally taken from https://arcanis.me/en/2015/10/17/cppcheck-and-clang-format
 # additional target to perform clang-format run, requires clang-format
+# Some additions have been made to better suit cszb-scoreboard's usage
 
-# get all project files
-file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+file(GLOB PROTO_FILES "*.proto")
+file(GLOB_RECURSE MAIN_SOURCE_FILES src/*.cpp include/*.h)
+file(GLOB_RECURSE TEST_SOURCE_FILES test/*.cpp test/*.h)
+
+set(ALL_SOURCE_FILES ${MAIN_SOURCE_FILES} ${TEST_SOURCE_FILES} ${PROTO_FILES})
 
 add_custom_target(
 	clangformat
