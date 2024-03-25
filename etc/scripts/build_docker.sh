703b16a5848f22c892ba305ac66fcbbf6070130e
Tracy Beck <tgbeck@acm.org>
2024-03-25 12:06:24 -0400

    Only build amd64 by default -- this is more likely what I want most of the time.  I could add this as a flag, and may do so in the future.

--- etc/scripts/build_docker.sh
+++ etc/scripts/build_docker.sh
@@ -3,12 +3,15 @@
 # simple script which just builds all of our standard docker containers.  I run
 # this sometimes just to pre-cache all of the containers for future executions.
 
+export PLATFORMS=linux/amd64
+# If using a detached builder, you can change to PLATFORMS=linux/amd64,linux/arm64
+
 # Actually builds everything
-docker buildx build --platform=linux/amd64,linux/arm64 --target=build_all .
+docker buildx build --platform=${PLATFORMS} --target=build_all .
 
 # Create some tags, for convenience. 
-docker buildx build --target=standard_test  --platform=linux/amd64,linux/arm64 -t scoreboard . && \
-docker buildx build --target=macos_test     --platform=linux/amd64,linux/arm64 -t osxcross   . && \
-docker buildx build --target=code_clean     --platform=linux/amd64,linux/arm64 -t code_clean .
+docker buildx build --target=standard_test  --platform=${PLATFORMS} -t scoreboard . && \
+docker buildx build --target=macos_test     --platform=${PLATFORMS} -t osxcross   . && \
+docker buildx build --target=code_clean     --platform=${PLATFORMS} -t code_clean .
 
 
