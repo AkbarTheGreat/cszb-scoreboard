4ebac9e08574b5593fe3b072d2ad83d05c54cc37
Tracy Beck <tgbeck@acm.org>
2024-06-27 16:16:39 -0400

    Change our typescript implementation to use protobufs for communication.

--- autoupdate_server/app/src/routes/VersionRoutes.ts
+++ autoupdate_server/app/src/routes/VersionRoutes.ts
@@ -2,18 +2,32 @@ import Url from 'url';
 
 import HttpStatusCodes from '@src/constants/HttpStatusCodes';
 import VersionService from '@src/services/VersionService';
-import { Version } from '@src/models/Version';
+import { GetLatestResponse, GetInfoResponse, GetAllResponse, GetUpdateURLsResponse } from '@proto/version_info';
 import { IReq, IRes } from './types/express/misc';
 
 
-// **** Functions **** //
+// **** Functions **** 
+
+async function sendBinaryProto(res: IRes, data: Uint8Array, messageType: string, httpStatus: HttpStatusCodes) {
+  res.writeHead(httpStatus, {
+    'Content-Type': `application/x-protobuf; messageType="${messageType}".`,
+    'Content-Length': data.length
+  });
+  res.end(Buffer.from(data), 'binary');
+  return res;
+};
 
 /**
  * Get latest Version
  */
 async function getLatest(_: IReq, res: IRes) {
   const latest = await VersionService.getLatest();
-  return res.status(HttpStatusCodes.OK).json({ 'latest': latest });
+  return await sendBinaryProto(
+    res,
+    GetLatestResponse.toBinary(latest),
+    GetLatestResponse.typeName,
+    latest.error ? HttpStatusCodes.NOT_FOUND : HttpStatusCodes.OK
+  );
 }
 
 /**
@@ -22,7 +36,12 @@ async function getLatest(_: IReq, res: IRes) {
 async function getInfo(req: IReq<{ params: { version: string } }>, res: IRes) {
   const version = req.params.version;
   const info = await VersionService.getInfo(version);
-  return res.status(HttpStatusCodes.OK).json({ 'version': info });
+  return await sendBinaryProto(
+    res,
+    GetInfoResponse.toBinary(info),
+    GetInfoResponse.typeName,
+    info.error ? HttpStatusCodes.NOT_FOUND : HttpStatusCodes.OK
+  );
 }
 
 /**
@@ -30,7 +49,12 @@ async function getInfo(req: IReq<{ params: { version: string } }>, res: IRes) {
  */
 async function getAll(_: IReq, res: IRes) {
   const versions = await VersionService.getAll();
-  return res.status(HttpStatusCodes.OK).json({ 'versions': versions });
+  return await sendBinaryProto(
+    res,
+    GetAllResponse.toBinary(versions),
+    GetAllResponse.typeName,
+    versions.error ? HttpStatusCodes.NOT_FOUND : HttpStatusCodes.OK
+  );
 }
 
 /**
@@ -39,15 +63,23 @@ async function getAll(_: IReq, res: IRes) {
 async function getUpdateURLs(req: IReq<{ params: { version: string, release: string } }>, res: IRes) {
   const version = req.params.version;
   const release = req.params.release;
-  const sub_urls = await VersionService.getUpdateURLs(version, release);
-  const urls = sub_urls.map(u =>
-    Url.format({
-      protocol: req.protocol,
-      host: req.get('host'),
-      pathname: u,
-    })
+  const initialResponse = await VersionService.getUpdateURLs(version, release);
+  const response: GetUpdateURLsResponse = {
+    urls: initialResponse.urls.map(u =>
+      Url.format({
+        protocol: req.protocol,
+        host: req.get('host'),
+        pathname: u,
+      })
+    ),
+    error: initialResponse.error
+  };
+  return await sendBinaryProto(
+    res,
+    GetUpdateURLsResponse.toBinary(response),
+    GetUpdateURLsResponse.typeName,
+    response.error ? HttpStatusCodes.NOT_FOUND : HttpStatusCodes.OK
   );
-  return res.status(HttpStatusCodes.OK).json({ 'urls': urls, });
 }
 
 // **** Export default **** //
