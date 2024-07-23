74693bf73e51b6e42a7a4aa7e1c00818b18e2126
Tracy Beck <tgbeck@acm.org>
2024-07-23 15:57:03 -0400

    Add a path to get the URLs needed to download an update and add static serving of the update files.

--- autoupdate_server/app/src/services/VersionService.ts
+++ autoupdate_server/app/src/services/VersionService.ts
@@ -33,10 +33,22 @@ async function getAll(): Promise<string[] | null> {
   return VersionRepo.getAll();
 }
 
+async function getUpdateURLs(version: string, release: string): Promise<string[]> {
+  const urls = await VersionRepo.getUpdateURLs(version, release);
+  if (urls == null) {
+    throw new RouteError(
+      HttpStatusCodes.NOT_FOUND,
+      VERSION_NOT_FOUND_ERR,
+    );
+  }
+  return urls;
+}
+
 // **** Export default **** //
 
 export default {
   getAll,
   getInfo,
   getLatest,
+  getUpdateURLs,
 } as const;
