53a331e4d64b1203784a35089824a10595ded061
Tracy Beck <tgbeck@acm.org>
2026-06-20 14:43:23 -0400

    Tweak local coverage runs a bit.
    This should also fix nightly coverage runs in Github.

--- etc/scripts/coverage.sh
+++ etc/scripts/coverage.sh
@@ -2,11 +2,9 @@
 
 # Use docker to build coverage for the tests and places it in out/cov
 
-docker buildx build --target=standard_test -t scoreboard .
+docker buildx build --load --target=standard_test -t scoreboard .
 
 docker run -v ${PWD}/out/cov:/cszb-scoreboard/out/Linux/Coverage/cszb-scoreboard-coverage --rm -it scoreboard \
     bash -c \
     'cmake --preset Linux-Coverage && \
-    cmake --build --preset Linux-Coverage'
-
-
+    cmake --build --parallel --preset Linux-Coverage'
