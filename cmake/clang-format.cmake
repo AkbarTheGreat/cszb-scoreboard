12f7741eae3fb087ef102fe66d3711199679a8e7
Tracy Beck <tgbeck@acm.org>
2026-06-27 11:02:52 -0400

    Phase 1 of Cleanup with Antigravity.
    Antigravity has suggested some CMake changes, which all look sane.  I'm working on having it clean up the overall Docker/Cmake strategies I've used in the past in order to tidy up what has become a bit of a snarl.
    
    I'm not sure I'm 100% in love with making the tests all as large as they are, but it might be better for maintenance than to have individual unit tests be tiny.  We'll see.

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,18 +1,25 @@
+# cmake/clang-format.cmake : CMake target for clang-format formatting
+# 
 # Originally taken from https://arcanis.me/en/2015/10/17/cppcheck-and-clang-format
 # additional target to perform clang-format run, requires clang-format
-# Some additions have been made to better suit cszb-scoreboard's usage
 
 file(GLOB PROTO_FILES "*.proto")
-file(GLOB_RECURSE MAIN_SOURCE_FILES src/*.cpp include/*.h)
-file(GLOB_RECURSE TEST_SOURCE_FILES test/*.cpp test/*.h)
+file(GLOB_RECURSE MAIN_SOURCE_FILES CONFIGURE_DEPENDS src/*.cpp include/*.h)
+file(GLOB_RECURSE TEST_SOURCE_FILES CONFIGURE_DEPENDS test/*.cpp test/*.h)
 
 set(ALL_SOURCE_FILES ${MAIN_SOURCE_FILES} ${TEST_SOURCE_FILES} ${PROTO_FILES})
 
+# Find clang-format executable dynamically
+find_program(CLANG_FORMAT_EXECUTABLE clang-format)
+if(NOT CLANG_FORMAT_EXECUTABLE)
+	# Fallback if not found in standard paths, assuming it might be in user PATH
+	set(CLANG_FORMAT_EXECUTABLE "clang-format")
+endif()
+
 add_custom_target(
 	clangformat
-	COMMAND /usr/bin/clang-format
+	COMMAND ${CLANG_FORMAT_EXECUTABLE}
 	-style=file
 	-i
 	${ALL_SOURCE_FILES}
 )
-
