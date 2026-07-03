693578fe843c8e219ed89551d36a2b2136d5f53e
Tracy Beck <tgbeck@acm.org>
2026-07-02 21:11:16 -0400

    Update the script to start a MacOS "VM"
    Yeah, it's just a docker image, but it's one of the easier ways I've seen to get a Mac OS running on my machine in a VM-like fashion.  So let's pretend it's a VM, for the sake of argument.

--- etc/scripts/osx_start_vm.sh
+++ etc/scripts/osx_start_vm.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Command from https://hub.docker.com/r/sickcodes/docker-osx but with a volume for the scoreboard added.
+# Command from https://hub.docker.com/r/sickcodes/docker-osx for ease of use.
+# I tried attaching a volume to just share the app, but it doesn't work great.
 
-docker run -dit \
-	--device /dev/kvm \
-	-p 50922:10022 \
-	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-e "DISPLAY=${DISPLAY:-:0.0}" \
-	-e GENERATE_UNIQUE=true \
-	sickcodes/docker-osx:auto
-
+docker run -it \
+    --device /dev/kvm \
+    -p 50922:10022 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e "DISPLAY=${DISPLAY:-:0.0}" \
+    -e GENERATE_UNIQUE=true \
+    -e CPU='Haswell-noTSX' \
+    -e CPUID_FLAGS='kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on' \
+    -e MASTER_PLIST_URL='https://raw.githubusercontent.com/sickcodes/osx-serial-generator/master/config-custom-sonoma.plist' \
+    -e SHORTNAME=tahoe \
+    sickcodes/docker-osx:latest
 
