a6530072f680ea582cff8cf5f54dd36dd95b16de
Tracy Beck <tgbeck@acm.org>
2026-07-05 11:03:27 -0400

    Removing these osx vm scripts.
    I now build ARM binaries, and this project only runs x86 VMs.  As such, they sort of imply a solution that just doesn't work for me at the moment, so removing them for clarity.

--- etc/scripts/osx_resume_vm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Command from https://hub.docker.com/r/sickcodes/docker-osx for ease of use.
-# I tried attaching a volume to just share the app, but it doesn't work great.
-
-docker start -ai osx-desktop
--- etc/scripts/osx_vm_copy_scoreboard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp -P 50922 -r out/osxcross_package/CszbScoreboard.app user@localhost:/Users/user/Desktop/
-
